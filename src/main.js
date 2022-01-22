5d1f117346129475c178bff60ea726ba2d1bddf4
David Mahbubi <ulrichdavid0370@gmail.com>
2022-01-22 22:33:42 +0700

    Updated package.json

--- src/main.js
+++ src/main.js
@@ -25,9 +25,6 @@ import RefreshButton from './components/Common/RefreshButton';
 // Colxx Component Add
 import Colxx from './components/Common/Colxx';
 
-// Perfect Scrollbar Add
-import vuePerfectScrollbar from 'vue-perfect-scrollbar';
-
 import { getCurrentLanguage } from './utils';
 
 import Notiflix from 'notiflix';
@@ -63,7 +60,6 @@ Vue.use(Notifications);
 Vue.component('piaf-breadcrumb', Breadcrumb);
 Vue.component('b-refresh-button', RefreshButton);
 Vue.component('b-colxx', Colxx);
-Vue.component('vue-perfect-scrollbar', vuePerfectScrollbar);
 
 Vue.config.productionTip = false;
 
