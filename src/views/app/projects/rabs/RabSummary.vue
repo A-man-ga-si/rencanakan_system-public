ed34dcd3739b232279aeb467110d7177216d1132
David Mahbubi <ulrichdavid0370@gmail.com>
2022-03-26 19:45:45 +0700

    Added padding to "terbilang"

--- src/views/app/projects/rabs/RabSummary.vue
+++ src/views/app/projects/rabs/RabSummary.vue
@@ -87,7 +87,9 @@
                 <h3>Terbilang</h3>
               </td>
               <td class="text-right">
-                <h3>{{ convertAlphabeuticalNum }} RUPIAH</h3>
+                <h3 style="padding-left: 70px">
+                  {{ convertAlphabeuticalNum }} RUPIAH
+                </h3>
               </td>
             </tr>
           </table>
