a4d315d062923179b01ea2d687e5c40c7b555f7b
Filbert Hartawan <filberthtwn@gmail.com>
2024-06-01 17:28:34 +0700

    fix: form behavior on master AHS form

--- src/components/Master/Ahs/AddAhs.vue
+++ src/components/Master/Ahs/AddAhs.vue
@@ -4,13 +4,16 @@
     :ref="modalId"
     :title="'Tambah AHS'"
     no-close-on-backdrop
+    @close="onTapCancelButton(modalId)"
   >
     <div
       class="labeled-select position-relative d-inline-block mb-4"
-      style="width: 100%"
+      :class="{ 'disabled': isSubmitting }"
+      style="width: 100%;"
     >
       <span class="px-1"> Group AHS</span>
       <v-select
+        :disabled=isSubmitting
         label="name"
         :reduce="ahs => ahs.id"
         :options="ahsGroup"
@@ -19,22 +22,37 @@
       </v-select>
     </div>
     <ValidationInput
+      :disabled=isSubmitting
       class="mb-4"
       label="Kode"
       field-name="id"
       v-model="form.id"
     />
     <ValidationInput
+      :disabled=isSubmitting
       class="mb-4"
       label="Nama"
       field-name="name"
       v-model="form.name"
     />
     <template slot="modal-footer">
-      <b-button @click.prevent="submitForm" variant="primary" class="mr-1">
-        {{ $t('modal.save-bt') }}
+      <b-button
+        :disabled=!isFormFilled
+        :style="buttonStyle"
+        @click.prevent="onTapSaveButton"
+        variant="primary"
+        class="mr-1"
+      >
+        <LoaderCircle v-show="isSubmitting"/>
+        <span v-show="!isSubmitting">
+          {{ $t('modal.save-bt') }}
+        </span>
       </b-button>
-      <b-button variant="secondary" @click="hideModal(modalId)">
+      <b-button
+        :style="buttonStyle"
+        variant="secondary"
+        @click="onTapCancelButton(modalId)"
+      >
         {{ $t('modal.cancel-bt') }}
       </b-button>
     </template>
@@ -46,11 +64,16 @@
   import validationMixin from '@/mixins/validation-mixins';
   import { mapActions } from 'vuex';
   import { Notify } from 'notiflix';
+  import LoaderCircle from '@/components/Common/LoaderCircle.vue';
 
   export default {
     mixins: [validationMixin],
     data: () => ({
       modalId: 'add-ahs-modal',
+      isSubmitting: false,
+      buttonStyle: {
+        height: '42px', width: '100px'
+      },
       form: {
         id: '',
         name: '',
@@ -67,29 +90,42 @@
         },
       ]
     }),
+    computed: {
+      isFormFilled() {
+        return this.form.groups != ''
+          && this.form.name != ''
+          && this.form.id != ''
+      }
+    },
     methods: {
       ...mapActions(['storeAhs']),
-      hideModal(refname) {
+      onTapCancelButton(refname) {
+        this.resetForm();
         this.$refs[refname].hide();
       },
-      resetForm() {
-        this.form.id = '';
-        this.form.name = '';
-      },
-      async submitForm() {
+      async onTapSaveButton() {
+        this.isSubmitting = true;
         try {
           this.resetInvalid();
-          const data = await this.storeAhs(this.form);
-          this.hideModal(this.modalId);
+          await this.storeAhs(this.form);
+          this.$refs[this.modalId].hide();
           this.$emit('ahs-added');
           Notify.success('Sukses Membuat AHS Baru');
+          this.isSubmitting = false;
           this.resetForm();
         } catch (err) {
+          this.isSubmitting = false;
           this.checkForInvalidResponse(err);
         }
       },
+      resetForm() {
+        this.form.groups = '';
+        this.form.id = '';
+        this.form.name = '';
+      }
     },
     components: {
+      LoaderCircle,
       ValidationInput,
     },
   };
